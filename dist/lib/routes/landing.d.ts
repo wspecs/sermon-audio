aab352aeb0f8ec3d06f63c260a1e8314fd00e6fd
Wendly Saintil <wendly@wendly-macbookpro2.roam.corp.google.com>
2019-06-03 20:07:49 -0700

    apply ignore

--- dist/lib/routes/landing.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class LandingController {
-    landing(res: any): void;
-}
--- dist/test/router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai_1 = require("chai");
-describe('word parser', () => {
-    beforeEach(() => { });
-    it('initializes', () => {
-        chai_1.expect('').to.equal('');
-    });
-});
